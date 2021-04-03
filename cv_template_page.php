e45b8eb5899ec05d1018a4e021cc66de37e989c8
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-04-03 19:13:40 +0800

    Update cv_template_page.php

--- cv_template_page.php
+++ cv_template_page.php
@@ -3,23 +3,22 @@
 Template Name: CV
 */
 ?>
-
-<?php include (TEMPLATEPATH . '/small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?> 
+<?php include (TEMPLATEPATH . 'starter/small_header');  /* Tells WordPress to include the smaller header header.php*/?> 
 <div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
             <section class="col-sm-7 maintext">				
     <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
 	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 <div class="post single-page">
 		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
 
 		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
+<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
+<?php the_content(); ?>
+<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
         </div><!--postcontet-->
 		
 	</div><!--post class-->
@@ -27,15 +26,15 @@ Template Name: CV
    
 <?php endwhile; ?>
 	
-	<?php else: ?>
+<?php else: ?>
 	<div class="post">
 		<p>Sorry, no posts found.</p>
 	</div><!--post-->
-	<?php endif; ?>
+<?php endif; ?>
 	</article><!---->
 </section>
            
      </div>
     </div>
   </div>
-  <?php get_footer(); ?>
+<?php get_footer(); ?>
