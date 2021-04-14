acb96fea0b7df88727644fb8bc40c8a96ba0cc14
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-04-14 18:49:25 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,50 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header();/* Tells WordPress to include header.php */ ?>
+
+    <section class="container-fluid aboutbg text-center">
+        <div class="container">
+<!--placeholder for about me  -->
+
+            <div class="blocker"></div>
+
+        </div>
+    </section>
+
+
+
+    <section class="container-fluid articlesbg">
+        <div class="container">
+            <div class="row">
+                <!--  a row that gives us access to the BS columns-->
+                <div class="article-title text-center">
+                    <h3>LATEST ARTICLES</h3>
+                </div>
+
+<div class="col-md-4 article-cent"> 
+
+<!--           Placeholder for col 1                 -->
+             
+              </div>
+            </div>
+            <div class="col-md-4 article-cent">
+            <!--           Placeholder for col 2                 -->
+             
+
+            </div>
+
+
+            <div class="col-md-4 article-cent"> 
+            <!--           Placeholder for col 1                 -->
+
+           
+
+        </div><!-- row-->
+        </div><!-- container-->
+    </section><!-- container-fluid-->
+<?php get_footer();/* Tells WordPress to include footer.php */ ?>
+
+
