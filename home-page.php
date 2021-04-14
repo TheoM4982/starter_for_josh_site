d445a00871fbd5ab6943a2fd4a9050a4b80df29c
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-04-14 18:57:39 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,7 +8,7 @@
     <section class="container-fluid aboutbg text-center">
         <div class="container">
 <!--placeholder for about me  -->
-
+<h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
             <div class="blocker"></div>
 
         </div>
