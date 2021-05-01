d1a922f4941a4cf5c0f12a9fc353e32c16939236
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-05-01 11:32:35 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
      wp_enqueue_style( 'tester', get_stylesheet_directory_uri() . '/css/animate.min.css');
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
-wp_enque_script ('wow', get_stylesheet_directory_uri() . '/js/wow.min.js', array(), '', true ();
+wp_enque_script ('wow', get_stylesheet_directory_uri() . '/js/wow.min.js', array(), true );
 }
  
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
